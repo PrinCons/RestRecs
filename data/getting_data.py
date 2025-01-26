0713d6e28056af2f11f9a1bdd576f11c590c4c00
PrinCons <115244073+PrinCons@users.noreply.github.com>
2025-01-25 22:46:28 -0500

    initial commit

---
+++ data/getting_data.py
@@ -0,0 +1,9 @@
+import kagglehub
+
+# Download latest version
+# path = kagglehub.dataset_download("rayhan32/trip-advisor-newyork-city-restaurants-dataset-10k")
+# print("--HERE--")
+# print(path)
+# path = kagglehub.dataset_download("beridzeg45/nyc-restaurants")
+# path = kagglehub.dataset_download("anoopjohny/new-york-restaurant-menus-and-details")
+path = kagglehub.dataset_download("new-york-city/nyc-inspections")
